01a3ef834162f9362a0163352ea80ab05c27682a
Rubens778 <rqosja04@gmail.com>
2024-08-17 11:21:47 +0200

    initial commit

---
+++ src/main/java/com/example/car_rental_management_app/CarRentalManagementAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.car_rental_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalManagementAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/car_rental_management_app/CarRentalManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_rental_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
